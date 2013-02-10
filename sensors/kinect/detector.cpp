d9c0d19e82b0e6bb338259ae5f36ea4df534d697
Terrence Cole <terrence@zettabytestorage.com>
2013-02-10 10:48:46 -0800

    Convert tabs to spaces and parse cli using TCLAP.

--- sensors/kinect/detector.cpp
+++ sensors/kinect/detector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <tclap/CmdLine.h>
 #include "Network.h"
 #include "Kinect.h"
 
@@ -7,31 +8,28 @@ using namespace std;
 
 static void run(const string &name, const string &controller)
 {
-	Network link(name);
-	Kinect kinect(&link);
-	
-	cout << "Started" << endl;
-	kinect.loop();
-	cout << "Finished" << endl;
+    Network link(name);
+    Kinect kinect(&link);
+
+    cout << "Started" << endl;
+    kinect.loop();
+    cout << "Finished" << endl;
 }
 
 int main(int argc, char **argv)
 {
-	if (argc <= 1) {
-		cout << "Arg1 must be NAME" << endl;
-		return 1;
-	}
-	string name(argv[1]);
+    TCLAP::CmdLine cmd("Opens a kinect and dispatches events to a controller.", ' ', "0.0.0");
+    TCLAP::ValueArg<string> nameArg("n","name","Name of this sensor.",true,"","string");
+    TCLAP::ValueArg<string> hostArg("a","address","Hostname of the controller.",true,"","ip address");
+    cmd.add(hostArg);
+    cmd.add(nameArg);
+    cmd.parse(argc, argv);
 
-	string controller(DefaultController);
-	if (argc > 2)
-		controller = string(argv[2]);
-
-	try {
-		run(name, controller);
-	} catch(KinectError e) {
-		cerr << "KinctError- " << e.message() << endl;
-		return 1;
-	}
-	return 0;
+    try {
+        run(nameArg.getValue(), hostArg.getValue());
+    } catch(KinectError e) {
+        cerr << "KinctError- " << e.message() << endl;
+        return 1;
+    }
+    return 0;
 }
