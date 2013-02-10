5ac156d7fb4be3aa15bb807a8baff896bc6be58a
Terrence Cole <terrence@zettabytestorage.com>
2013-02-10 12:47:12 -0800

    Hook up to our registration mechanism and show results.

--- sensors/kinect/try.cpp
+++ sensors/kinect/try.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unistd.h>
 
+#include <tclap/CmdLine.h>
+
 #include "Kinect.h"
 #include "Math.h"
 
@@ -8,25 +10,42 @@ using namespace std;
 
 class Events : public IKinectEventSink
 {
-  public:
-    virtual void detectedNewUser(int uid) {}
-    virtual void addUser(int uid) {}
-    virtual void removeUser(int uid) {}
+    Matrix44T M;
 
-    virtual void setPosition(int uid, float x, float y, float z) {
-        //cout << x << ", " << y << ", " << z << endl;
-        Vec3T sensor(x, z, y);
-        sensor = Matrix44T::scale(1/25.4) * sensor;
-        TransformT tr(-35, 0, -30, 0, 0, 73);
-        cout << tr.matrix() * sensor << endl;
+  public:
+    Events(const vector<Number> &registration) {
+        size_t pos = 0;
+        for (size_t i = 0; i < 4; ++i)
+            for (size_t j = 0; j < 4; ++j)
+                M.set(i, j, registration[pos++]);
+    }
+
+    virtual void detectedNewUser(int) {}
+    virtual void addUser(int) {}
+    virtual void removeUser(int) {}
+
+    virtual void setPosition(int, float x, float y, float z) {
+        cout << M * Vec3T(x, y, z) << endl;
     }
 };
 
 int main(int argc, char **argv)
 {
-    Events evt;
+    TCLAP::CmdLine cmd("See if a particular matrix works well.", ' ', "0.0.0");
+    TCLAP::UnlabeledMultiArg<Number> registrationArg("registration",
+                    "A 16 element vector of matrix elements.", true, "VEC", false);
+    cmd.add(registrationArg);
+    cmd.parse(argc, argv);
+
+    vector<Number> registration = registrationArg.getValue();
+    if (registration.size() != 16) {
+        cerr << "The registration vector must be 16 long." << endl;
+        return 1;
+    }
+
+    Events evt(registration);
     Kinect kinect(&evt);
-    
+
     cout << "Waiting for user." << endl;
     kinect.loop();
 
