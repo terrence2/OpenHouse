667567423e5da8670746b7879e9375ea301d5811
Terrence Cole <terrence@zettabytestorage.com>
2013-02-10 10:47:47 -0800

    Convert tabs to space and move consts directly into Network.

--- sensors/kinect/Network.h
+++ sensors/kinect/Network.h
@@ -8,27 +8,26 @@
 
 #include "Kinect.h"
 
-static const char *DefaultController = "gorilla";
-static const int SensorPort = 31975;
-static const int ControlPort = 31976;
-
 class Network : public IKinectEventSink
 {
   public:
-	Network(const std::string &name);
+    static const int SensorPort = 31975;
+    static const int ControlPort = 31976;
 
-	void detectedNewUser(int uid);
-	void addUser(int uid);
-	void removeUser(int uid);
-	void setPosition(int uid, float x, float y, float z);
+    Network(const std::string &name);
 
-	void checkControlSock();
+    void detectedNewUser(int uid);
+    void addUser(int uid);
+    void removeUser(int uid);
+    void setPosition(int uid, float x, float y, float z);
+
+    void checkControlSock();
 
   protected:
-	std::string mName;
-	zmq::context_t mCtx;
-	zmq::socket_t mSensorSock;
-	zmq::socket_t mControlSock;
+    std::string mName;
+    zmq::context_t mCtx;
+    zmq::socket_t mSensorSock;
+    zmq::socket_t mControlSock;
 };
 
 #endif // _Network_h__
