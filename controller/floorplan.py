20fcc3ffbe2852f3c5ba5450d8933a1c20caffc8
Terrence Cole <terrence@zettabytestorage.com>
2013-02-10 10:13:46 -0800

    Add registration to floorplan.

--- controller/floorplan.py
+++ controller/floorplan.py
@@ -1,95 +1,106 @@
+from collections import defaultdict
 from sensors import Sensor
 from servos import Servo
 
+def registration_to_matrix(nums:[float]):
+    return None
+
 class Portal:
-	def __init__(self, target, width, height, x, y):
-		self.target = target
-		self.width = width # float in meters
-		self.height = height
-		self.x = x
-		self.y = y
+    def __init__(self, target, width, height, x, y):
+        self.target = target
+        self.width = width # float in meters
+        self.height = height
+        self.x = x
+        self.y = y
 
 class Room:
-	"""
-	An axis-aligned rectangular extent in a floorplan.
-	"""
-	def __init__(self, name, width, height):
-		super().__init__()
-		self.name = name
-		self.width = width
-		self.height = height
+    """
+    An axis-aligned rectangular extent in a floorplan.
+    """
+    def __init__(self, name, width, height):
+        super().__init__()
+        self.name = name
+        self.width = width
+        self.height = height
 
-		self.portals = []
-		self.sensors = {}
-		self.servos = {}
+        self.portals = []
+        self.sensors = {}
+        self.servos = {}
 
-	def add_portal_to(self, other, size, position):
-		p = Portal(other, size[0], size[1], position[0], position[1])
-		self.portals.append(p)
-		return p
+    def add_portal_to(self, other, size, position):
+        p = Portal(other, size[0], size[1], position[0], position[1])
+        self.portals.append(p)
+        return p
 
-	def add_servo(self, servo:Servo):
-		assert servo.name not in self.servos
-		self.servos[servo.name] = servo
+    def add_servo(self, servo:Servo):
+        assert servo.name not in self.servos
+        self.servos[servo.name] = servo
 
-	def add_sensor(self, sensor:Sensor, position:(float, float)):
-		assert sensor.name not in self.sensors
-		self.sensors[sensor.name] = {'position': position, 'sensor': sensor}
+    def add_sensor(self, sensor:Sensor, position:(float, float), registration:[float]):
+        assert sensor.name not in self.sensors
+        self.sensors[sensor.name] = {'position': position,
+                                     'matrix': registration_to_matrix(register),
+                                     'sensor': sensor}
 
 class FloorPlan:
-	"""
-	Contains Rooms filled with Sensors and Servos and links them together into
-	a conceptual space.
-	"""
-	def __init__(self, name):
-		super().__init__()
-		self.name = name
-		self.rooms = {}
-		self.sensors = {}
-		self.servos = {}
-	
-	def add_room(self, name, width, height) -> Room:
-		assert name not in self.rooms
-		self.rooms[name] = Room(name, width, height)
-		return self.rooms[name]
+    """
+    Contains Rooms filled with Sensors and Servos and links them together into
+    a conceptual space.
+    """
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.rooms = {}
+        self.sensors = {}
+        self.servos = {}
+        self.sensorToRooms = defaultdict(list) # {str: [str]}
 
-	def get_room(self, name:str) -> Room:
-		return self.rooms[name]
+    def add_room(self, name, width, height) -> Room:
+        assert name not in self.rooms
+        self.rooms[name] = Room(name, width, height)
+        return self.rooms[name]
 
-	def add_servo(self, servo:Servo, roomName:str):
-		if servo.name not in self.servos:
-			self.servos[servo.name] = servo
-		assert servo is self.servos[servo.name]
-		self.rooms[roomName].add_servo(servo)
+    def get_room(self, name:str) -> Room:
+        return self.rooms[name]
 
-	def get_servo(self, name:str):
-		return self.servos[name]
+    def add_servo(self, servo:Servo, roomName:str):
+        if servo.name not in self.servos:
+            self.servos[servo.name] = servo
+        assert servo is self.servos[servo.name]
+        self.rooms[roomName].add_servo(servo)
 
-	def all_servos(self):
-		return self.servos.values()
+    def get_servo(self, name:str):
+        return self.servos[name]
 
-	def add_sensor(self, sensor:Sensor, roomName:str, position:(float,float)):
-		if sensor.name not in self.sensors:
-			self.sensors[sensor.name] = sensor
-		assert sensor is self.sensors[sensor.name]
-		self.rooms[roomName].add_sensor(sensor, position)
-	
-	def get_sensor(self, name:str):
-		return self.sensors[name]
+    def all_servos(self):
+        return self.servos.values()
 
-	def all_sensors(self):
-		return self.sensors.values()
+    def add_sensor(self, sensor:Sensor, roomName:str, position:(float,float), registration:[float]):
+        if sensor.name not in self.sensors:
+            self.sensors[sensor.name] = sensor
+        assert sensor is self.sensors[sensor.name]
+        self.rooms[roomName].add_sensor(sensor, position, registration)
+        self.sensorToRooms[sensor.name].append(roomName)
 
-	def handle_sensor_message(self, json):
-		if 'name' not in json:
-			print("Dropping invalid message: no name")
-			return
+    def get_sensor(self, name:str):
+        return self.sensors[name]
 
-		name = json['name']
-		if name not in self.sensors:
-			print("Got control message from unknown sensor: {}".format(name))
-			return
+    def all_sensors(self):
+        return self.sensors.values()
 
-		sensor = self.sensors[name]
-		sensor.handle_sensor_message(json)
+    def rooms_with_sensor(self, sensor:Sensor) -> [Room]:
+        return [self.rooms[name] for name in self.sensorToRooms[sensor.name]]
+
+    def handle_sensor_message(self, json):
+        if 'name' not in json:
+            print("Dropping invalid message: no name")
+            return
+
+        name = json['name']
+        if name not in self.sensors:
+            print("Got control message from unknown sensor: {}".format(name))
+            return
+
+        sensor = self.sensors[name]
+        sensor.handle_sensor_message(json)
 
--- controller/main.py
+++ controller/main.py
@@ -11,110 +11,116 @@ from ruleset import RuleSet
 from network import Network
 
 class HouseRules(RuleSet):
-	def event_BedroomKinect_MAYBEADDUSER(self, sensor):
-		# If we were OFF before, then turn on, otherwise, we may already be in
-		# a higher-priority state.
-		if len(sensor.users) == 1:
-			self.floorplan.get_servo('BedLightStrip').turn_on_full()
+    def event_BedroomKinect_MAYBEADDUSER(self, sensor):
+        # If we were OFF before, then turn on, otherwise, we may already be in
+        # a higher-priority state.
+        if len(sensor.users) == 1:
+            self.floorplan.get_servo('BedLightStrip').turn_on_full()
 
-	def event_BedroomKinect_ADDUSER(self, sensor):
-		# This is only here in case we missed the MAYBEADDUSER.
-		if len(sensor.users) == 1:
-			self.floorplan.get_servo('BedLightStrip').turn_on_full()
+    def event_BedroomKinect_ADDUSER(self, sensor):
+        # This is only here in case we missed the MAYBEADDUSER.
+        if len(sensor.users) == 1:
+            self.floorplan.get_servo('BedLightStrip').turn_on_full()
 
-	def event_BedroomKinect_REMOVEUSER(self, sensor):
-		# Last one out, hit the lights.
-		if not sensor.users:
-			self.floorplan.get_servo('BedLightStrip').turn_off_full()
-	
-	def event_BedroomKinect_POSITION(self, sensor, pos):
-		#self.floorplan.get_servo('BedLightStrip').send_test_message()
-		pass
+    def event_BedroomKinect_REMOVEUSER(self, sensor):
+        # Last one out, hit the lights.
+        if not sensor.users:
+            self.floorplan.get_servo('BedLightStrip').turn_off_full()
+
+    def event_BedroomKinect_POSITION(self, sensor, sensorPos):
+        for room in self.rooms_with_sensor(sensor):
+            roomPos = room.map_sensor_position_to_room_position(sensorPos)
+        #self.floorplan.get_servo('BedLightStrip').send_test_message()
+        pass
 
 def build_floorplan() -> FloorPlan:
-	"""
-	  0,0         24'
-		+----------+-----------+
-		|  11'3"   |           |
-		-----------+  Bedroom  |
-		| P  |  :  |           | 12'7"
-		|-------+  |           |
-		|       |HW|  12'4"   k|------+
-		|       +  +-----------+      |
-	25' | Kitch :              | Porch|
-		|       :              |      |
-		|             Living   |11'11"|
-		|Dining                |      |
-		|        Entry         |------+
-		+----------------------+  6'7"
-	"""
-	def m(s):
-		feet = 0.305 # m
-		inch = feet / 12. # m
-		matches = re.match(r'(-?\d+)\'(?:(-?\d+)")?', s.strip())
-		if matches:
-			if len(matches.groups()) == 2:
-				return int(matches.group(1)) * feet
-			return int(matches.group(1)) * feet + int(matches.group(2)) * inch
+    """
+      0,0         24'
+        +----------+-----------+
+        |  11'3"   |           |
+        -----------+  Bedroom  |
+        | P  |  :  |           | 12'7"
+        |-------+  |           |
+        |       |HW|  12'4"   k|------+
+        |       +  +-----------+      |
+    25' | Kitch :              | Porch|
+        |       :              |      |
+        |             Living   |11'11"|
+        |Dining                |      |
+        |        Entry         |------+
+        +----------------------+  6'7"
+    """
+    def m(s):
+        feet = 0.305 # m
+        inch = feet / 12. # m
+        matches = re.match(r'(-?\d+)\'(?:(-?\d+)")?', s.strip())
+        if matches:
+            if len(matches.groups()) == 2:
+                return int(matches.group(1)) * feet
+            return int(matches.group(1)) * feet + int(matches.group(2)) * inch
 
-	flat = FloorPlan('TrainedMonkeyStudios')
-	rules = HouseRules(flat)
+    flat = FloorPlan('TrainedMonkeyStudios')
+    rules = HouseRules(flat)
 
-	# Add all logical rooms.
-	bathroom = flat.add_room('Bathroom', m(''' 11'3" '''),  m(''' 4'9" '''))
-	bedroom = flat.add_room( 'Bedroom',  m(''' 12'4" '''),  m(''' 12'7" '''))
-	pantry = flat.add_room(  'Pantry',   m(''' 4'7" '''),   m(''' 2'6" '''))
-	laundry = flat.add_room( 'Laundry',  m(''' 3'1" '''),   m(''' 2'6" '''))
-	hallway = flat.add_room( 'Hallway',  m(''' 3' '''),     m(''' 7'7" '''))
-	kitchen = flat.add_room( 'Kitchen',  m(''' 8'4" '''),   m(''' 8' '''))
-	entry = flat.add_room(   'Entry',    m(''' 3'8" '''),   m(''' 11'11" '''))
-	living = flat.add_room(  'Living',   m(''' 12'4"  '''), m(''' 11'11"  '''))
-	porch = flat.add_room(   'Porch',    m(''' 6'7" '''),   m(''' 11'11" '''))
-	dining = flat.add_room(  'Dining',   m(''' 7'7" '''),   m(''' 8'3" '''))
-	
-	# Connect our rooms.
-	doors = [
-		(('''2'4"''', '''6"'''), 'Bathroom', ('''8'11"''', '''4'6"'''), 'Hallway', ('''0'4"''', '''0'-3"''')), 
-		(('''6"''', '''2'6"'''), 'Bedroom', ('''3"''', '''9'10"'''), 'Hallway', ('''2'9"''', '''6'10"''')),
-	]
-	for size, name1, pos1, name2, pos2 in doors:
-		room1 = flat.get_room(name1)
-		room2 = flat.get_room(name2)
-		size = (m(size[0]), m(size[1]))
-		pos1 = (m(pos1[0]), m(pos1[1]))
-		pos2 = (m(pos2[0]), m(pos2[1]))
-		room1.add_portal_to(room2, size, pos1)
-		room2.add_portal_to(room1, size, pos2)
-	
-	# Add all the sensors.
-	sensors = [
-		(Kinect, 'BedroomKinect', '127.0.0.1', Network.DefaultSensorPort,
-				[('Bedroom', m(''' 12'4" '''), m(''' 12'7" '''), m(''' 6' '''))])
-	]
-	for cls, name, host, port, rooms in sensors:
-		s = cls(rules, name, (host, port))
-		for roomName, X, Y, Z in rooms:
-			flat.add_sensor(s, roomName, (X, Y, Z))
+    # Add all logical rooms.
+    bathroom = flat.add_room('Bathroom', m(''' 11'3" '''),  m(''' 4'9" '''))
+    bedroom = flat.add_room( 'Bedroom',  m(''' 12'4" '''),  m(''' 12'7" '''))
+    pantry = flat.add_room(  'Pantry',   m(''' 4'7" '''),   m(''' 2'6" '''))
+    laundry = flat.add_room( 'Laundry',  m(''' 3'1" '''),   m(''' 2'6" '''))
+    hallway = flat.add_room( 'Hallway',  m(''' 3' '''),     m(''' 7'7" '''))
+    kitchen = flat.add_room( 'Kitchen',  m(''' 8'4" '''),   m(''' 8' '''))
+    entry = flat.add_room(   'Entry',    m(''' 3'8" '''),   m(''' 11'11" '''))
+    living = flat.add_room(  'Living',   m(''' 12'4"  '''), m(''' 11'11"  '''))
+    porch = flat.add_room(   'Porch',    m(''' 6'7" '''),   m(''' 11'11" '''))
+    dining = flat.add_room(  'Dining',   m(''' 7'7" '''),   m(''' 8'3" '''))
 
-	# Add all servos.
-	servos = [
-		(LightStrip, 'BedLightStrip', '127.0.0.1', Network.DefaultServoPort, 'Bedroom')
-	]
-	for cls, name, host, port, room in servos:
-		s = cls(name, (host, port))
-		flat.add_servo
-	bedLightStrip = LightStrip('BedLightStrip', '127.0.0.1')
-	flat.add_servo(bedLightStrip, 'Bedroom')
+    # Connect our rooms.
+    doors = [
+        (('''2'4"''', '''6"'''), 'Bathroom', ('''8'11"''', '''4'6"'''), 'Hallway', ('''0'4"''', '''0'-3"''')),
+        (('''6"''', '''2'6"'''), 'Bedroom', ('''3"''', '''9'10"'''), 'Hallway', ('''2'9"''', '''6'10"''')),
+    ]
+    for size, name1, pos1, name2, pos2 in doors:
+        room1 = flat.get_room(name1)
+        room2 = flat.get_room(name2)
+        size = (m(size[0]), m(size[1]))
+        pos1 = (m(pos1[0]), m(pos1[1]))
+        pos2 = (m(pos2[0]), m(pos2[1]))
+        room1.add_portal_to(room2, size, pos1)
+        room2.add_portal_to(room1, size, pos2)
 
-	return flat
+    # Add all the sensors.
+    sensors = [
+        (Kinect, 'BedroomKinect', '127.0.0.1', Network.DefaultSensorPort,
+                [('Bedroom', m(''' 12'4" '''), m(''' 12'7" '''), m(''' 6'1" '''),
+                             [-0.0204035, -0.0147874, -0.0302495, 144.0,
+                               0.0335971, -0.0112752, -0.0171496, 147.0,
+                               0.00222178, 0.0347016, -0.0184625, 73.0,
+                               0.0, 0.0, 0.0, 1.0])])
+    ]
+    for cls, name, host, port, rooms in sensors:
+        s = cls(rules, name, (host, port))
+        for roomName, X, Y, Z, registration in rooms:
+            flat.add_sensor(s, roomName, (X, Y, Z), registration)
+
+    # Add all servos.
+    servos = [
+        (LightStrip, 'BedLightStrip', '127.0.0.1', Network.DefaultServoPort, 'Bedroom')
+    ]
+    for cls, name, host, port, room in servos:
+        s = cls(name, (host, port))
+        flat.add_servo(s, room)
+    #bedLightStrip = LightStrip('BedLightStrip', '127.0.0.1')
+    #flat.add_servo(bedLightStrip, 'Bedroom')
+
+    return flat
 
 def main():
-	floorplan = build_floorplan()
+    floorplan = build_floorplan()
 
-	network = Network(floorplan)
+    network = Network(floorplan)
 
-	return network.run()
+    return network.run()
 
 
 if __name__ == '__main__':
-	sys.exit(main())
+    sys.exit(main())
