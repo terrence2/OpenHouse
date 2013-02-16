4034e60222797a0eaaf0c5e8496efcd58842163d
Terrence Cole <terrence@zettabytestorage.com>
2013-02-16 09:51:16 -0800

    Move arduino connector to a separate module.

---
+++ servos/lightstrip/arduino.py
@@ -0,0 +1,57 @@
+import glob
+import serial
+import time
+
+class Arduino:
+    def __init__(self, devglob, baud):
+        self.devglob = devglob
+        self.baud = baud
+        self.tty = None
+        self.tty = self.connect()
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        if self.tty:
+            self.tty.close()
+            self.tty = None
+
+    def find_device_names(self):
+        i = 0
+        while True:
+            devices = glob.glob(self.devglob)
+            if devices:
+                return devices
+            if i == 0:
+                print("No devices like {} found: retrying every 10s".format(self.devglob))
+            i += 1
+            time.sleep(10)
+
+    def connect(self):
+        devnames = self.find_device_names()
+        assert devnames
+        tty = None
+        for name in devnames:
+            print("Trying to open arduino at: {}".format(name))
+            tty = serial.Serial(name, self.baud)
+            if not tty:
+                continue
+
+        if not tty:
+            print("Waiting 10s for arduino to appear in /dev")
+            time.sleep(10)
+            return self.connect()
+
+        print("Waiting 3s for arduino to reboot...")
+        time.sleep(3)
+        return tty
+
+    def write(self, data:bytes):
+        assert self.tty
+        try:
+            self.tty.write(data)
+        except serial.SerialException as e:
+            self.tty = self.connect()
+            return self.write(data)
+
--- servos/lightstrip/main.py
+++ servos/lightstrip/main.py
@@ -1,67 +1,12 @@
 #!/usr/bin/python3
+from arduino import Arduino
 import argparse
-import glob
-import serial
 import socket
 import sys
-import time
 import zmq
 
 DefaultControllerHost = 'gorilla'
 DefaultServoPort = 31978
-
-class Arduino:
-    def __init__(self, devglob, baud):
-        self.devglob = devglob
-        self.baud = baud
-        self.tty = None
-        self.tty = self.connect()
-
-    def __del__(self):
-        self.close()
-
-    def close(self):
-        if self.tty:
-            self.tty.close()
-            self.tty = None
-
-    def find_device_names(self):
-        i = 0
-        while True:
-            devices = glob.glob(self.devglob)
-            if devices:
-                return devices
-            if i == 0:
-                print("No devices like {} found: retrying every 10s".format(self.devglob))
-            i += 1
-            time.sleep(10)
-
-    def connect(self):
-        devnames = self.find_device_names()
-        assert devnames
-        tty = None
-        for name in devnames:
-            print("Trying to open arduino at: {}".format(name))
-            tty = serial.Serial(name, self.baud)
-            if not tty:
-                continue
-
-        if not tty:
-            print("Waiting 10s for arduino to appear in /dev")
-            time.sleep(10)
-            return self.connect()
-
-        print("Waiting 3s for arduino to reboot...")
-        time.sleep(3)
-        return tty
-
-    def write(self, data:bytes):
-        assert self.tty
-        try:
-            self.tty.write(data)
-        except serial.SerialException as e:
-            self.tty = self.connect()
-            return self.write(data)
 
 
 def main():
