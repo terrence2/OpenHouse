649e226f7e802e7367888c4f1955cd8609e7194b
Terrence Cole <terrence.d.cole@gmail.com>
2014-03-01 10:17:06 -0800

    Add an event system to the Abode.

--- server/mcp/abode.py
+++ server/mcp/abode.py
@@ -5,46 +5,100 @@ from mcp.dimension import Coord, Size
 
 class Area:
     def __init__(self, parent, name: str, position: Coord, size: Size):
-        self.parent_ = parent
-
+        # Intrinsic properties.
+        self.parent = parent
         self.name = name
         self.position = position
         self.size = size
 
+        # Sub-divisions of this space.
         self.subareas_ = {}  #: {Area.name: Area}
 
+        # The set of properties attached to this region and the set of callbacks
+        # directed at events on those properties.
         self.properties_ = {}  #: {str: any}
+        self.listeners_ = {}  #: {str: {str: [callable]}} property -> event -> calls
 
-        self.listeners_ = {}  #: {str: [callable]}
+    def path(self):
+        """
+        Construct and return the path that when passed to Abode.lookup will return
+        this Area.
+        """
+        components = [self.name]
+        parent = self.parent
+        while parent is not None:
+            components.append(parent.name)
+            parent = parent.parent
+        components = reversed(components)
+        return '/' + '/'.join(components)
 
     def create_subarea(self, name: str, position: Coord, size: Size):
+        """
+        Instantiate and return a new Area that is a sub-area of the current area.
+        """
         area = Area(self, name, position, size)
         self.subareas_[area.name] = area
         return area
 
     def subarea(self, name: str):
+        """
+        Return the named subarea.
+        """
         return self.subareas_[name]
 
-    def set(self, key: str, val: object):
-        if key not in self.properties_:
-            self.send_event('newProp', key, val)
-        elif val != self.properties_[key]:
-            self.send_event('propChanged', key, val)
-        self.properties_[key] = val
-        self.send_event('propUpdated', key)
+    def listen(self, prop_name: str, event_name: str, callback: callable):
+        """
+        Attach an event listener to call the given callback when the given
+        event name occurs on the given property name.
+        """
+        if prop_name not in self.listeners_:
+            self.listeners_[prop_name] = {}
+        if event_name not in self.listeners_[prop_name]:
+            self.listeners_[prop_name][event_name] = []
+        self.listeners_[prop_name][event_name].append(callback)
 
-    def send_event(self, name: str, key: str, **kwargs):
-        if key not in
-        event = Event(.name, self, **kwargs)
+    def set(self, prop_name: str, prop_value: object):
+        """
+        Update the value of a property on this area.
+        """
+        if prop_name not in self.properties_:
+            self.properties_[prop_name] = None
+            self.send_event(prop_name, 'propertyAdded', prop_value)
+        if prop_value != self.properties_[prop_name]:
+            self.send_event(prop_name, 'propertyChanged', prop_value)
+        self.properties_[prop_name] = prop_value
+        self.send_event(prop_name, 'propertyTouched', prop_value)
 
+    def get(self, prop_name: str, default: object=None):
+        """
+        Get the value of a property.
+        """
+        if prop_name not in self.properties_:
+            if default is not None:
+                return default
+            raise KeyError("Property {} not in {}".format(prop_name, self.path()))
+        return self.properties_[prop_name]
+
+    def send_event(self, prop_name: str, event_name: str, prop_value: object=None):
+        """
+        Send an arbitrary event. An event must be fore a property name, but the property
+        does not have to actually exist in the tree.
+        """
+        if prop_name not in self.listeners_:
+            return
+        if event_name not in self.listeners_[prop_name]:
+            return
+        event = Event(event_name, self, prop_name, prop_value)
+        for callback in self.listeners_[prop_name][event_name]:
+            callback(event)
 
 
 class Event:
-    def __init__(self, name: str, target: Area, **kwargs):
-        self.name = name
+    def __init__(self, event_name: str, target: Area, prop_name: str, prop_value: object):
+        self.event_name = event_name
         self.target = target
-        for k, v in kwargs.items():
-            setattr(self, k, v)
+        self.property_name = prop_name
+        self.property_value = prop_value
 
 
 class Abode(Area):
@@ -57,7 +111,23 @@ class Abode(Area):
     At the moment, without some sort of automatic mapping software like google's Tango, it is
     typically more convenient to specify an Abode in terms of axis aligned bounding rectangles,
     so this is what we do. Rooms' position is relative to Abode, Areas' position is relative to
-    Room.
+    Room. There is no constraint that a subarea must be within the parent or not within some other
+    peer in the graph. Whether this is convenient or dangerous depends on the use.
+
+    Queries:
+        Every Area has a name, position, and size. Areas can be found via a path using |lookup|,
+        or found by name using |find|.
+
+    Properties and Listeners:
+        Arbitrary properties can be set on an area and queried later. Users of the Abode can
+        register to receive notifications of property changes via |listen|. Current, changing
+        a property triggers the following events:
+
+        propertyAdded - The property was set for the first time. Not generally terribly useful
+                        but included for completeness.
+        propertyChanged - Triggered when the value of a property changes.
+        propertyTouched - Triggered when the value of a property is set, but its value is the
+                          same as the value that was there previously.
     """
 
     def __init__(self, name: str):
--- server/test/test_abode.py
+++ server/test/test_abode.py
@@ -6,24 +6,96 @@ __author__ = 'terrence'
 
 
 class TestAbode(TestCase):
-    def test_create_room(self):
+    def build_abode(self):
         abode = Abode('Test')
         room1 = abode.create_room('Room1', Coord(0, 0), Size(10, 10, 8))
         room2 = abode.create_room('Room2', Coord(8, 8), Size(10, 10, 8))
         room3 = abode.create_room('Room3', Coord(16, 16), Size(10, 10, 8))
+        for room in [room1, room2, room3]:
+            area1 = room.create_subarea('Area1', Coord(0, 0), Size(10, 10, 8))
+            area2 = room.create_subarea('Area2', Coord(8, 8), Size(10, 10, 8))
+            area3 = room.create_subarea('Area3', Coord(16, 16), Size(10, 10, 8))
+        return (abode, room1, room2, room3, area1, area2, area3)
+
+    def test_create_room(self):
+        abode, room1, room2, room3, _, _, _ = self.build_abode()
         self.assertIs(room2, abode.room("Room2"))
         self.assertIs(room2, abode.lookup("/Test/Room2"))
         self.assertEqual(room2.position, Coord(8, 8))
         self.assertEqual(room2.size, Size(10, 10, 8))
 
     def test_create_area(self):
-        abode = Abode('Test')
-        room = abode.create_room('Room', Coord(0, 0), Size(10, 10, 8))
-        area1 = room.create_subarea('Area1', Coord(0, 0), Size(10, 10, 8))
-        area2 = room.create_subarea('Area2', Coord(8, 8), Size(10, 10, 8))
-        area3 = room.create_subarea('Area3', Coord(16, 16), Size(10, 10, 8))
-        self.assertIs(area2, room.subarea("Area2"))
-        self.assertIs(area2, abode.lookup("/Test/Room/Area2"))
+        abode, room1, room2, room3, area1, area2, area3 = self.build_abode()
+        self.assertIs(area2, room3.subarea("Area2"))
+        self.assertIs(area2, abode.lookup("/Test/Room3/Area2"))
         self.assertEqual(area2.position, Coord(8, 8))
         self.assertEqual(area2.size, Size(10, 10, 8))
+
+    def test_path(self):
+        abode, room1, room2, room3, area1, area2, area3 = self.build_abode()
+        self.assertEqual('/Test', abode.path())
+        self.assertEqual('/Test/Room1', room1.path())
+        self.assertEqual('/Test/Room2', room2.path())
+        self.assertEqual('/Test/Room3', room3.path())
+        self.assertEqual('/Test/Room3/Area1', area1.path())
+        self.assertEqual('/Test/Room3/Area2', area2.path())
+        self.assertEqual('/Test/Room3/Area3', area3.path())
+        for area in (abode, room1, room2, room3, area1, area2, area3):
+            self.assertIs(area, abode.lookup(area.path()))
+
+    def test_get_and_set(self):
+        props = {'foo': 'bar', 'hello': 'world'}
+        areas = self.build_abode()
+        for area in areas:
+            for key, val in props.items():
+                area.set(key, val)
+        for area in areas:
+            for key, val in props.items():
+                self.assertEqual(val, area.get(key))
+
+    def test_set_events(self):
+        call_count = 0
+        def receiver(expect_event: str, expect_name: str, expect_value: str):
+            nonlocal call_count
+            def callback(event):
+                nonlocal call_count
+                self.assertEqual(expect_event, event.event_name)
+                self.assertEqual(expect_name, event.property_name)
+                self.assertEqual(expect_value, event.property_value)
+                call_count += 1
+            return callback
+
+        props = {'foo': 'bar', 'hello': 'world'}
+        areas = self.build_abode()
+
+        # Check that initial update fires both added and changed.
+        for area in areas:
+            for key, val in props.items():
+                area.listen(key, 'propertyAdded', receiver('propertyAdded', key, val))
+                area.listen(key, 'propertyChanged', receiver('propertyChanged', key, val))
+        for area in areas:
+            for key, val in props.items():
+                area.set(key, val)
+        self.assertEqual(len(areas) * len(props) * 2, call_count)
+        for area in areas:
+            for key, val in props.items():
+                self.assertEqual(val, area.get(key))
+        self.assertEqual(len(areas) * len(props) * 2, call_count)  # No new calls when getting.
+
+        # Check that setting the key to the same value does not fire new added/changed events.
+        for area in areas:
+            for key, val in props.items():
+                area.set(key, val)
+        self.assertEqual(len(areas) * len(props) * 2, call_count)  # No new calls when getting.
+
+        # Add touch events and re-set to see if those fire correctly.
+        for area in areas:
+            for key, val in props.items():
+                area.listen(key, 'propertyTouched', receiver('propertyTouched', key, val))
+        for area in areas:
+            for key, val in props.items():
+                area.set(key, val)
+        self.assertEqual(len(areas) * len(props) * 3, call_count)
+
+
 
