1c1d0104c410752aaf46f1f7dcd22b2457ca3929
Terrence Cole <terrence.d.cole@gmail.com>
2016-02-07 09:12:41 -0800

    Get CLI running again, if not actually working.

--- oh_cli/oh_cli.py
+++ oh_cli/oh_cli.py
@@ -2,23 +2,33 @@
 """
 Simple example of a syntax-highlighted HTML input line.
 """
-import os.path
+import asyncio
 
-from threading import Lock
+from oh_shared.args import parse_default_args
+from oh_shared.log import enable_logging
+from oh_shared.home import Home
 
-from shared.home import Home
-
-from prompt_toolkit.contrib.repl import embed
-from bs4 import BeautifulSoup as bs
+#from prompt_toolkit.contrib.repl import embed
+#from bs4 import BeautifulSoup as bs
 
 
+@asyncio.coroutine
 def main():
-    home = Home((3, 0), Lock())
-    home.start()
-    embed(globals(), locals(), vi_mode=True, history_filename=os.path.expanduser("~/.oh_history.txt"))
-    home.exit()
-    home.join()
+    args = parse_default_args('Respond to sunrise and sunset states with dramatic fades.')
+    enable_logging(args.log_target, args.log_level)
+    home = yield from Home.connect((args.home_address, args.home_port))
+
+    while True:
+        query = input("> ")
+        foo = yield from home.query(query).run()
+        print(foo)
+
+    #embed(globals(), locals(), vi_mode=True, history_filename=os.path.expanduser("~/.oh_history.txt"))
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.get_event_loop().run_until_complete(main())
+    try:
+        asyncio.get_event_loop().run_forever()
+    except KeyboardInterrupt:
+        pass
