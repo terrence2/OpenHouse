6c7d5af11c6940d2bf680a821ef04f7912096235
Terrence Cole <terrence.d.cole@gmail.com>
2016-02-27 09:36:30 -0800

    Remove the ancient www dir. It served graphs like 3 rewrites ago.

--- www/dashboard/views.py
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the GNU General Public
-# License, version 3. If a copy of the GPL was not distributed with this file,
-# You can obtain one at https://www.gnu.org/licenses/gpl.txt.
-from django.shortcuts import render
-from django.template import loader
-
-
-def index(request):
-    return render(request, "dashboard.html")
