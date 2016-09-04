f08fc39a7d6acecd5a64d806629348c18cb397f3
Terrence Cole <terrence.d.cole@gmail.com>
2016-09-04 09:55:59 -0700

    Fairly reliable Hue light integration.

--- oh_hue/oh_hue.py
+++ oh_hue/oh_hue.py
@@ -5,7 +5,6 @@
 import asyncio
 import logging
 from hue.bridge import Bridge
-from hue.light import Light
 from oh_shared.args import parse_default_args
 from oh_shared.log import enable_logging
 from oh_shared.db import Connection, EventKind, Tree
@@ -36,31 +35,20 @@ async def main():
     # Subscribe to all light changes.
     async def on_color_changed(paths: [str], _: EventKind, context: str):
         names = names_from_color_paths(paths)
-        log.info("changed color of {} to {}".format(names, context))
         await bridge.set_lights_to_color(names, context)
     await tree.subscribe("/room/*/hue-*/*/color", on_color_changed)
 
+    return tree, bridge.task
 
-
-
-    """
-    res = yield from home.query("light[kind=hue], light[kind=hue-livingcolors]").run()
-    lights = []
-    for path, node in res.items():
-        try:
-            bridge, light_id = find_bridge_owning_light(bridges, node)
-            light = yield from Light.create(light_id, path, node, bridge, home)
-            lights.append(light)
-        except BridgeNotFound as ex:
-            log.error("Found light '{}' with no owning bridge. "
-                      "Please double-check the spelling."
-                      .format(ex.light_name))
-
-    # Show lights that may be unconfigured.
-    for bridge in bridges:
-        bridge.show_unqueried_lights()
-    """
 
 if __name__ == '__main__':
-    tree = asyncio.get_event_loop().run_until_complete(main())
-    asyncio.get_event_loop().run_forever()
+    tree, task = asyncio.get_event_loop().run_until_complete(main())
+    try:
+        asyncio.get_event_loop().run_until_complete(task)
+    except KeyboardInterrupt:
+        asyncio.get_event_loop().run_until_complete(tree.close())
+        task.cancel()
+        try:
+            asyncio.get_event_loop().run_until_complete(task)
+        except asyncio.CancelledError:
+            pass
