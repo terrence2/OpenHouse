826c806f49a8d0a646fa5ccc2c17a0e002331d1d
Terrence Cole <terrence.d.cole@gmail.com>
2016-09-21 15:14:01 -0700

    Organize our test code a bit better.

--- oh_db/test/test_lowlevel.py
+++ oh_db/test/test_lowlevel.py
@@ -8,58 +8,6 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_initial_tree():
-    """
-    Do some work using async calls.
-    """
-    with run_server():
-        async with make_connection() as tree:
-            assert await tree.list_directory("/") == []
-
-
-@pytest.mark.asyncio
-async def test_tree_async():
-    """
-    Do some work using async calls.
-    """
-    with run_server():
-        async with make_connection() as tree:
-            futures = []
-            children = "abcdefghijklmnopqrstuvwxyz"
-            for i, a in enumerate(children):
-                if i % 2 == 0:
-                    futures.append(await tree.create_directory_async("/", a))
-                else:
-                    # Note that this is safe because we're using TCP under the hood. If we ever do anything non-serial,
-                    # this test will need to change pretty dramatically.
-                    futures.append(await tree.create_file_async("/", a))
-                    futures.append(await tree.set_file_async("/" + a, a))
-            await asyncio.gather(*futures)
-
-            future = await tree.list_directory_async("/")
-            result = await future
-            assert "".join(sorted(result.children)) == children
-
-            futures = []
-            for i, a in enumerate(children):
-                if i % 2 == 1:
-                    futures.append(await tree.get_file_async("/" + a))
-            results = await asyncio.gather(*futures)
-            assert "".join(sorted([rv.data for rv in results])) == children[1::2]
-
-            futures = []
-            vowels = "aeiou"
-            for a in vowels:
-                futures.append(await tree.remove_node_async("/", a))
-            await asyncio.gather(*futures)
-
-            future = await tree.list_directory_async("/")
-            result = await future
-            assert "".join(sorted(result.children)) == \
-                   "bcdfghjklmnpqrstvwxyz"
-
-
-@pytest.mark.asyncio
 async def test_subscribe_same_client_layout():
     """
     Ensure that subscriptions work on a directory and that we can:
@@ -311,21 +259,4 @@ async def test_subscribe_glob_multi():
             await tree.set_matching_files("/**/bar", "c")
             expect = (2, ["/a/a/foo", "/a/b/foo", "/a/c/foo"], db.EventKind.changed, "c")
             await tree.set_matching_files("/**/foo", "c")
-
-# test_subscribe_glob_intersecting
-
-
-@pytest.mark.asyncio
-async def test_set_glob_basic():
-    """
-    Ensure that globs only match matching files.
-    """
-    with run_server():
-        async with make_connection() as tree:
-            for name in "abcd":
-                await tree.create_file("/", name)
-            await tree.set_matching_files("/*", "hello")
-            data = await tree.get_matching_files("/*")
-            for name in "abcd":
-                assert data["/" + name] == "hello"
 
