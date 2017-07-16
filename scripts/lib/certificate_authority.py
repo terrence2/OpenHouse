16abae118a81c07fa34014914a166bd4af2e3c19
Terrence Cole <terrence.d.cole@gmail.com>
2017-07-15 23:31:00 -0700

    Finish moving CA bits into lib.

--- scripts/lib/certificate_authority.py
+++ scripts/lib/certificate_authority.py
@@ -126,17 +126,17 @@ extendedKeyUsage = critical, OCSPSigning
 """
 
 
-def make_ca_directory_structure():
+def _make_ca_directory_structure():
     make_directory("CA")
     with change_directory("CA"):
-        make_sub_ca_directory_structure("root")
-        write_ca_configuration(cert_dir="root", name="ca", policy="policy_strict")
+        _make_sub_ca_directory_structure("root")
+        _write_ca_configuration(cert_dir="root", name="ca", policy="policy_strict")
 
-        make_sub_ca_directory_structure("intermediate")
-        write_ca_configuration(cert_dir="intermediate", name="intermediate", policy="policy_loose")
+        _make_sub_ca_directory_structure("intermediate")
+        _write_ca_configuration(cert_dir="intermediate", name="intermediate", policy="policy_loose")
 
 
-def make_sub_ca_directory_structure(root: str):
+def _make_sub_ca_directory_structure(root: str):
     assert "/" not in root
     make_directory(root)
     with change_directory(root):
@@ -147,7 +147,7 @@ def make_sub_ca_directory_structure(root: str):
         make_directory("private", 0o700)
 
 
-def write_ca_configuration(**params):
+def _write_ca_configuration(**params):
     with change_directory(params['cert_dir']):
         config = CONFIG_TEMPLATE.format(**params)
         replace_file("openssl.cnf", config)
@@ -157,6 +157,8 @@ def write_ca_configuration(**params):
 
 
 def make_certificate_authority(key_size: int, expire: int, key_security: str, x509_security: str):
+    _make_ca_directory_structure()
+
     params = {
         'key_size': key_size,
         'expire': expire,
@@ -164,52 +166,92 @@ def make_certificate_authority(key_size: int, expire: int, key_security: str, x5
         'x509_security': x509_security
     }
 
-    # Generate the root CA key pair.
-    #
-    # You should burn this to a CD after this is done and save it in a vault in
-    # case you need to regenerate the following later. Or just regenerate the
-    # entire chain, since it's all under your own control anyway.
-    with change_directory("root"):
-        # Generate the CA signing key.
-        call("openssl genrsa {key_security} -out private/ca.key.pem {key_size}", **params)
-        os.chmod("private/ca.key.pem", 0o400)
+    with change_directory("CA"):
+        # Generate the root CA key pair.
+        #
+        # You should burn this to a CD after this is done and save it in a vault in
+        # case you need to regenerate the following later. Or just regenerate the
+        # entire chain, since it's all under your own control anyway.
+        with change_directory("root"):
+            # Generate the CA signing key.
+            call("openssl genrsa {key_security} -out private/ca.key.pem {key_size}", **params)
+            os.chmod("private/ca.key.pem", 0o400)
 
-        # Create the CA root certificate.
-        call("""openssl req -config openssl.cnf -key private/ca.key.pem
-                                -new -x509 {x509_security} -days {expire}
-                                -subj /C=US/ST=CA/L=SB/O=Me/OU=OpenHouse/CN=certificate_authority/
-                                -extensions v3_ca
-                                -out certs/ca.cert.pem""", **params)
-        os.chmod("certs/ca.cert.pem", 0o444)
+            # Create the CA root certificate.
+            call("""openssl req -config openssl.cnf -key private/ca.key.pem
+                                    -new -x509 {x509_security} -days {expire}
+                                    -subj /C=US/ST=CA/L=SB/O=Me/OU=OpenHouse/CN=certificate_authority/
+                                    -extensions v3_ca
+                                    -out certs/ca.cert.pem""", **params)
+            os.chmod("certs/ca.cert.pem", 0o444)
 
-    # Make an intermediate signing CA key pair.
-    # This is the in-use signing key for daily use.
-    with change_directory("intermediate"):
-        # Generate the intermediate signing key.
-        call("openssl genrsa {key_security} -out private/intermediate.key.pem {key_size}", **params)
-        os.chmod("private/intermediate.key.pem", 0o400)
+        # Make an intermediate signing CA key pair.
+        # This is the in-use signing key for daily use.
+        with change_directory("intermediate"):
+            # Generate the intermediate signing key.
+            call("openssl genrsa {key_security} -out private/intermediate.key.pem {key_size}", **params)
+            os.chmod("private/intermediate.key.pem", 0o400)
 
-        # Create a certificate signing request for the intermediate.
-        call("""openssl req -config openssl.cnf -new {x509_security}
-                                -subj /C=US/ST=CA/L=SB/O=Me/OU=OpenHouse/CN=intermediate_authority/
-                                -key private/intermediate.key.pem
-                                -out csr/intermediate.csr.pem""", **params)
+            # Create a certificate signing request for the intermediate.
+            call("""openssl req -config openssl.cnf -new {x509_security}
+                                    -subj /C=US/ST=CA/L=SB/O=Me/OU=OpenHouse/CN=intermediate_authority/
+                                    -key private/intermediate.key.pem
+                                    -out csr/intermediate.csr.pem""", **params)
 
-    # Sign the intermediate CA with the root CA, generating the certificate.
-    call("""openssl ca -config root/openssl.cnf
-                           -extensions v3_intermediate_ca -batch
-                           -days {expire} -notext -md sha256
-                           -in intermediate/csr/intermediate.csr.pem
-                           -out intermediate/certs/intermediate.cert.pem""", **params)
-    os.chmod("intermediate/certs/intermediate.cert.pem", 0o444)
+        # Sign the intermediate CA with the root CA, generating the certificate.
+        call("""openssl ca -config root/openssl.cnf
+                               -extensions v3_intermediate_ca -batch
+                               -days {expire} -notext -md sha256
+                               -in intermediate/csr/intermediate.csr.pem
+                               -out intermediate/certs/intermediate.cert.pem""", **params)
+        os.chmod("intermediate/certs/intermediate.cert.pem", 0o444)
 
-    # Verify.
-    call("openssl verify -CAfile root/certs/ca.cert.pem intermediate/certs/intermediate.cert.pem")
+        # Verify.
+        call("openssl verify -CAfile root/certs/ca.cert.pem intermediate/certs/intermediate.cert.pem")
 
-    # Make a conjoined chain certificate.
-    with open("root/certs/ca.cert.pem", "r") as fp_root:
-        with open("intermediate/certs/intermediate.cert.pem", "r") as fp_intermediate:
-            with open("intermediate/certs/chain.cert.pem", "w") as fp_chain:
-                fp_chain.write(fp_root.read())
-                fp_chain.write(fp_intermediate.read())
+        # Make a conjoined chain certificate.
+        with open("root/certs/ca.cert.pem", "r") as fp_root:
+            with open("intermediate/certs/intermediate.cert.pem", "r") as fp_intermediate:
+                with open("intermediate/certs/chain.cert.pem", "w") as fp_chain:
+                    fp_chain.write(fp_root.read())
+                    fp_chain.write(fp_intermediate.read())
+
+
+def _make_certificate(name: str, extension: str, key_size: int, expire: int,
+                      key_security: str, x509_security: str):
+    assert os.path.isdir('CA')
+
+    params = {
+        'name': name,
+        'extension': extension,
+        'key_size': key_size,
+        'expire': expire,
+        'key_security': key_security,
+        'x509_security': x509_security
+    }
+    with change_directory("CA"):
+        with change_directory("intermediate"):
+            call("openssl genrsa {key_security} -out private/{name}.key.pem {key_size}", **params)
+            os.chmod("private/{name}.key.pem".format(**params), 0o400)
+            call("""openssl req -config openssl.cnf -new {x509_security}
+                                        -subj /C=US/ST=CA/L=SB/O=Me/OU=OpenHouse/CN={name}/
+                                        -key private/{name}.key.pem
+                                        -out csr/{name}.csr.pem""", **params)
+        call("""openssl ca -config intermediate/openssl.cnf
+                               -extensions {extension} -batch
+                               -days {expire} -notext -md sha256
+                               -in intermediate/csr/{name}.csr.pem
+                               -out intermediate/certs/{name}.cert.pem""",
+             **params)
+        os.chmod("intermediate/certs/{name}.cert.pem".format(**params), 0o444)
+
+
+def make_server_certificate(name: str, key_size: int, expire: int, key_security: str, x509_security: str):
+    _make_certificate(name, 'server_cert', key_size=key_size, expire=expire,
+                      key_security=key_security, x509_security=x509_security)
+
+
+def make_client_certificate(name: str, key_size: int, expire: int, key_security: str, x509_security: str):
+    _make_certificate(name, 'usr_cert', key_size=key_size, expire=expire,
+                      key_security=key_security, x509_security=x509_security)
 
--- scripts/make_certificate_chain.py
+++ scripts/make_certificate_chain.py
@@ -5,8 +5,9 @@ import sys
 sys.path.append(os.path.join(os.getcwd(), "scripts"))
 from lib import call, change_directory
 from lib.certificate_authority import\
-    make_ca_directory_structure,\
-    make_certificate_authority
+    make_certificate_authority,\
+    make_server_certificate,\
+    make_client_certificate
 
 
 SERVERS = [
@@ -27,55 +28,18 @@ CLIENTS = [
     "oh_supervisor",
 ]
 
-
-def make_certificate(name: str, extension: str, key_size: int, expire: int,
-                     key_security: str, x509_security: str):
-    params = {
-        'name': name,
-        'extension': extension,
-        'key_size': key_size,
-        'expire': expire,
-        'key_security': key_security,
-        'x509_security': x509_security
-    }
-    with change_directory("intermediate"):
-        call("openssl genrsa {key_security} -out private/{name}.key.pem {key_size}", **params)
-        os.chmod("private/{name}.key.pem".format(**params), 0o400)
-        call("""openssl req -config openssl.cnf -new {x509_security}
-                                    -subj /C=US/ST=CA/L=SB/O=Me/OU=OpenHouse/CN={name}/
-                                    -key private/{name}.key.pem
-                                    -out csr/{name}.csr.pem""", **params)
-    call("""openssl ca -config intermediate/openssl.cnf
-                           -extensions {extension} -batch
-                           -days {expire} -notext -md sha256
-                           -in intermediate/csr/{name}.csr.pem
-                           -out intermediate/certs/{name}.cert.pem""",
-         **params)
-    os.chmod("intermediate/certs/{name}.cert.pem".format(**params), 0o444)
-
-
-def make_certificates():
-    for server in SERVERS:
-        make_certificate(server, 'server_cert', key_size=4096, expire=100*365,
-                         key_security='', x509_security='-nodes')
-
-    for client in CLIENTS:
-        make_certificate(client, 'usr_cert', key_size=4096, expire=100*365,
-                         key_security='', x509_security='-nodes')
-
-
 def main():
     if not os.path.isfile("LICENSE"):
         print("This program expects to be run from the root OpenHouse checkout directory.")
         return 1
 
-    make_ca_directory_structure()
+    make_certificate_authority(key_size=8192, expire=100*365, key_security='', x509_security='-nodes')
 
-    with change_directory("CA"):
-        make_certificate_authority(key_size=8192, expire=100*365, key_security='', x509_security='-nodes')
+    for server in SERVERS:
+        make_server_certificate(server, key_size=4096, expire=100*365, key_security='', x509_security='-nodes')
 
-    with change_directory("CA"):
-        make_certificates()
+    for client in CLIENTS:
+        make_client_certificate(client, key_size=4096, expire=100*365, key_security='', x509_security='-nodes')
 
     return 0
 
